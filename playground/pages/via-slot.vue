41f6e7f2762bd5bd627f0b70124a5d3b9e0465ae
Zadigo <pendenquejohn@gmail.com>
2025-11-12 14:23:16 +0100

    New page via-slot

---
+++ playground/pages/via-slot.vue
@@ -0,0 +1,25 @@
+<template>
+  <section>
+    <nuxt-container>
+      <nuxt-card>
+        <nuxt-input v-model="username" placeholder="Email" />
+        <nuxt-input v-model="password" placeholder="Password" />
+
+        <nuxt-login v-model:username-field="username" v-model:password-field="password">
+          <template #default="{ login: login2 }">
+            <nuxt-button @click="() => login2()">
+              Login via Slot
+            </nuxt-button>
+          </template>
+        </nuxt-login>
+      </nuxt-card>
+    </nuxt-container>
+  </section>
+</template>
+
+<script lang="ts" setup>
+import NuxtLogin from '../../src/runtime/components/NuxtLogin.vue'
+
+const username = ref('')
+const password = ref('')
+</script>
