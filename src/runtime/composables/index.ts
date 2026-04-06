745f9af9b75c9d78f70e6d0b90f32ca612605d4e
Zadigo <pendenquejohn@gmail.com>
2026-04-07 00:36:06 +0200

    Improve typing

--- src/runtime/composables/index.ts
+++ src/runtime/composables/index.ts
@@ -164,8 +164,8 @@ export const useNuxtAuthentication = createGlobalState(() => {
     /**
      * Function which can be used to verify the access token
      * when the Nuxt app or page is mounted
-     * @param verificationKey : What key to check in the response to consider the token invalid
-     * @param verificationValue : What to check for in the response in order to consider the token invalid
+     * @param _verificationKey What key to check in the response to consider the token invalid
+     * @param _verificationValue What to check for in the response in order to consider the token invalid
      * @example ```ts
      * // In app.vue
      * const { verify } = useNuxtAuthentication()
