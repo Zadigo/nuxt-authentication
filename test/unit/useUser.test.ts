73d9fceb42704e79f475f2c0ba67ee94f92c79ba
Zadigo <pendenquejohn@gmail.com>
2026-07-13 09:49:39 +0200

    Remove test for getUserId

--- test/unit/useUser.test.ts
+++ test/unit/useUser.test.ts
@@ -9,11 +9,11 @@ describe('composables', () => {
     expect(isAuthenticated).toBeDefined()
   })
 
-  it('should be able to get the user id', async () => {
-    const { getUserId } = useUser()
-    const data = await getUserId()
+  // it('should be able to get the user id', async () => {
+  //   const { getUserId } = useUser()
+  //   const data = await getUserId()
 
-    expect(getUserId).toBeTypeOf('function')  
-    expect(data).toHaveProperty('user_id')
-  })
+  //   expect(getUserId).toBeTypeOf('function')  
+  //   expect(data).toHaveProperty('user_id')
+  // })
 })
